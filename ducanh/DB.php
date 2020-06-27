6b7572203ed3dad25d2cc16bd27beb02c4ffc0e0
ducanhtrung1 <61597969+ducanhtrung1@users.noreply.github.com>
2020-06-27 21:11:26 +0700

    Update DB.php

--- ducanh/DB.php
+++ ducanh/DB.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO('pgsql:host=ec2-52-87-135-240.compute-1.amazonaws.com;dbname=dc7sdr5vold8kr','charset=utf8wzpwspsasuqxgj','baa04e77a98dfa1c92cf258ff906a448ccfce3e6abffc4177e3df0c9cbaa80cc');
+    $db = new PDO('pgsql:host=ec2-18-214-119-135.compute-1.amazonaws.com;dbname=dcn2l34onuh0g3','gnhyavjavygcrj','9fc64148d6d2e83d43e60c20567f140c6b2f3ae59cdf57e1876289acba55b0bd');
     return $db;
 }catch (Exception $e){
     echo "Connect fail";
